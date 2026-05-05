9f55c65adfc9ffc2708308e12a983fba073746c6
hahuy <haquanghuy2006abc@gmail.com>
2026-05-05 13:57:58 +0700

    first commit

--- src/main/java/com/example/canteen/repository/WalletRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.canteen.repository;
-
-import com.example.canteen.entity.Wallet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WalletRepository
-        extends JpaRepository<Wallet, Long> {
-}
